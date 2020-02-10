7ba338719db0a194362ab0aaa928e02412a83f99
Vertigo <vertigo117@yandex.ru>
2020-02-10 16:40:43 +0300

    First Commit

---
+++ WebAPI/WebAPI/Controllers/ApplicationUserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> userManager;
+        private SignInManager<ApplicationUser> signInManager;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        //POST: /api/ApplicationUser/Register
+        public async Task<object> PostApplicationUser(ApplicationUserModel model)
+        {
+            ApplicationUser applicationUser = new ApplicationUser() { UserName = model.UserName, Email = model.Email, FullName = model.FullName };
+
+            try
+            {
+                var result = await userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpGet]
+        [Route("Index")]
+        public string Get()
+        {
+            return ("It's working!");
+        }
+    }
+}
