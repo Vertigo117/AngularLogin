ef8275e827bf7cdcb0e22666ccc9078f8ece71a1
Vertigo <vertigo117@yandex.ru>
2020-02-11 17:28:50 +0300

    angular project rebuilt

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -3,24 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 
-const routes: Routes = [
-  {
-    path: '', redirectTo: '/user/registration', pathMatch:'full'
 
-  },
+const routes: Routes = [
+  { path: '', redirectTo: '/user/registration', pathMatch: 'full'},
   {
-    path: 'user', component: UserComponent,
+    path: 'user', component:UserComponent,
     children: [
-      { path: 'registration', component:RegistrationComponent}
-    ]
-  },
+      { path: 'registration', component:RegistrationComponent }
+  ]
+  }
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { RegistrationComponent } from './user/registration/registration.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,6 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    NgbModule,
     AppRoutingModule
   ],
   providers: [],
